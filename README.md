e541d68aef623233c9ababe7278a6ee40e75f5c0
Nicholas Whittaker <nicholas@nicholas.cloud>
2021-09-18 15:10:52 +1000

    Use Inter font

--- README.md
+++ README.md
@@ -6,10 +6,14 @@ I like tweeting the music I listen to from time to time. Usually I do this by sc
 
 ![](./screenshot.png)
 
+Uses the [Inter font](https://github.com/rsms/inter), somewhat a slimmed down fork of [og-image](https://github.com/vercel/og-image).
+
 ## Setup
 
 You can clone this project and deploy it to [Vercel](https://vercel.com/). From there you can use Apple Shortcuts to grab the song currently playing on your device, generate a cover, and tweet it.
 
 Make sure to swap out the URL for your own Vercel deployment. (`https://country-backroads-*.vercel.app/api/`)
+
+If you want to limit access, requests will be blocked if the `SECRET` environment variable is set in Vercel and incoming request lack the matching `Secret` header value.
 
 ![](./shortcut.png)
--- api/index.js
+++ api/index.js
@@ -1,10 +1,31 @@
 import core from "puppeteer-core";
 import chromium from "chrome-aws-lambda";
+import got from "got";
 
 export default async (req, res) => {
+  // Reject request without a secret if one is set
   if (process.env.SECRET && req.headers.secret !== process.env.SECRET) {
     throw new Error("Bad request");
   }
+
+  // Fetch fonts, prepare browser
+  const [interRegular, interBold, interItalic] = await Promise.all([
+    got(
+      "https://raw.githubusercontent.com/rsms/inter/master/docs/font-files/Inter-Regular.woff2"
+    )
+      .buffer()
+      .then((buffer) => buffer.toString("base64")),
+    got(
+      "https://raw.githubusercontent.com/rsms/inter/master/docs/font-files/Inter-Bold.woff2"
+    )
+      .buffer()
+      .then((buffer) => buffer.toString("base64")),
+    got(
+      "https://raw.githubusercontent.com/rsms/inter/master/docs/font-files/Inter-Italic.woff2"
+    )
+      .buffer()
+      .then((buffer) => buffer.toString("base64")),
+  ]);
   const browser = await core.launch({
     headless: true,
     args: chromium.args,
@@ -12,9 +33,37 @@ export default async (req, res) => {
     defaultViewport: { width: 1800, height: 900 },
   });
   const page = await browser.newPage();
+
+  // Generate page and capture screenshot
   // prettier-ignore
   await page.setContent(`
     <!DOCTYPE html>
+    <head>
+        <style>
+            @font-face {
+                font-family: "Inter";
+                font-style: normal;
+                font-weight: normal;
+                src: url(data:font/woff2;charset=utf-8;base64,${interRegular.toString('base64')}) format('woff2')
+            }
+            @font-face {
+                font-family: "Inter";
+                font-style: normal;
+                font-weight: bold;
+                src: url(data:font/woff2;charset=utf-8;base64,${interBold.toString('base64')}) format('woff2')
+            }
+            @font-face {
+                font-family: "Inter";
+                font-style: italic;
+                font-weight: normal;
+                src: url(data:font/woff2;charset=utf-8;base64,${interItalic.toString('base64')}) format('woff2')
+            }
+
+            p {
+              font-family: 'Inter', monospace
+            }
+        </style>
+    </head>
     <body style="padding: 180px 90px; display: flex; margin: 0; align-items: center; justify-content: center;">
         <img style="height: 540px; box-shadow: 12px 12px 8px #ccc; margin-right: 120px; display: inline" src="data:image/png;base64,${req.body.artwork}" alt="No image" />
         <p style="font-size: 48px; display: inline; width: auto;">
